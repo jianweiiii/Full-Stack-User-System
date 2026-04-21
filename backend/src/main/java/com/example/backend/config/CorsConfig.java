c121cbeac40acda31e04f34eeeb4e0067cd0aaa5
jianweiiii <jianweii97@hotmail.com>
2026-04-21 21:24:14 +0800

    Backend : fixed cors error

--- backend/src/main/java/com/example/backend/config/CorsConfig.java
+++ backend/src/main/java/com/example/backend/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
 
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",                // local frontend
-                "https://your-frontend.vercel.app"      // deployed frontend (replace later)
+                "full-stack-user-system.vercel.app"      // deployed frontend (replace later)
         ));
 
         config.setAllowedMethods(List.of(
